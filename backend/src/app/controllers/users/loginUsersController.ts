9eaf57597aed7f8ed2f0fa2d2cf04d32b7c6bb29
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-08-25 18:56:52 -0300

    message: Endpoint de login finalizado

--- backend/src/app/controllers/users/loginUsersController.ts
+++ backend/src/app/controllers/users/loginUsersController.ts
@@ -3,20 +3,35 @@ import type { ReqAuthLogin } from '../../../types/users/users.ts';
 import { prisma } from '../../../../database/config.ts';
 
 export const LoginUserControl = async (req: Request, res: Response) => {
+  try {
     const { email, password } = req.body as ReqAuthLogin;
 
     const user = await prisma.user.findUnique({
-        where: {email}
+        where: {email},
+        select: {
+            name: true,
+            email: true,
+            password: true
+        }
     });
 
+    if (!user) {
+        return res.status(401).json({message: 'E-mail ou Senha incorretas.'});
+    }
 
+    if (user.password !== password) {
+        return res.status(401).json({ message: 'E-mail ou senha incorretos.' });
+    }
 
 
     return res.status(200).json({
-        message: 'Login realizado com sucesso.',
+        message: `${user.name}, login realizado com sucesso.`,
         user: {
-            email,
-            password
+            name: user.name,
+            email :user.email,
         },
     });
+  } catch(err) {
+        return res.status(500).json({message: 'Erro ao realizar o login, tente novamente.'})
+  }
 };
