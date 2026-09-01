783fe382a2a6e48aa7567c550bf49a12ac23e119
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-09-01 18:22:25 -0300

    Criando a função para comparar a senha com o hash

--- backend/src/app/jwt/AuthToken.ts
+++ backend/src/app/jwt/AuthToken.ts
@@ -1,34 +1,42 @@
 import type { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 import type { AuthToken } from '../../types/users/users.ts';
+import bcrypt from 'bcrypt';
 import { prisma } from '../../../database/config.ts';
+import jwt from 'jsonwebtoken';
 
 
 export const authUserToken = async (req: Request, res: Response) => {
 
-    const { name, email } = req.body as AuthToken;
+    const { name, email, password } = req.body as AuthToken;
 
-    let whereData = {}
-
-    if(email) {
-      whereData = { email };
-    } else if (name) {
-      whereData = { name };
-    } else {
-      return res.status(401).json({message: 'We need E-mail or Passowrd.'})
+    if(!email || !name || !password) {
+      return res.status(400).json({message: 'E-mail, Name and Password are required.'});
     }
 
     const verifyUser = await prisma.user.findFirst({
-      where: whereData,
+      where: {
+        name,
+        email
+      },
       select: {
-        name: true,
-        email: true
+        name : true,
+        email : true,
+        password: true
       }
     });
 
     if(!verifyUser) {
-        return res.status(401).json({error: 'User not found!'});
+      return res.status(401).json({error: 'User not found!'});
+    };
+
+    const checkPassword = await bcrypt.compare(password, verifyUser.password);
+
+    if (!checkPassword) {
+      return res.status(401).json({error: 'Invalid password!'});
     };
     
-    return res.status(200).json({user: verifyUser});
+    return res.status(200).json({user: {
+      name: verifyUser.name,
+      email: verifyUser.email
+    }});
 };
